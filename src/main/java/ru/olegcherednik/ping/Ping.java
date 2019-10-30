4d05c455469ee7ae69e693b4230d2bc2cdc1b579
Oleg Cherednik <oleg_cherednik@epam.com>
2019-10-30 20:20:33 +0300

    Add ping thread daemon

--- src/main/java/ru/olegcherednik/ping/Ping.java
+++ src/main/java/ru/olegcherednik/ping/Ping.java
@@ -10,7 +10,6 @@ import javax.swing.JOptionPane;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 import java.awt.AWTException;
-import java.awt.CheckboxMenuItem;
 import java.awt.Image;
 import java.awt.Menu;
 import java.awt.MenuItem;
@@ -19,179 +18,186 @@ import java.awt.SystemTray;
 import java.awt.TrayIcon;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author Oleg Cherednik
  * @since 30.10.2019
  */
-public class Ping {
+public class Ping implements ActionListener {
 
-    private static final IconManager iconManager = IconManager.getInstance();
+    private static final int SC_OK = 200;
 
-    public static void main(String[] args) {
-        /* Use an appropriate Look and Feel */
-//        try {
+    private static final String ID_ON = "on";
+    private static final String ID_OFF = "off";
+
+    private static final String CMD_MENU_ABOUT = "about";
+    private static final String CMD_MENU_ERROR = "error";
+    private static final String CMD_MENU_WARNING = "warning";
+    private static final String CMD_MENU_INFO = "info";
+    private static final String CMD_MENU_NONE = "none";
+    private static final String CMD_MENU_EXIT = "exit";
+    private static final String CMD_MAIN = "main";
+
+    private final Image imageOn = createImage(ID_ON);
+    private final Image imageOff = createImage(ID_OFF);
+
+    private final TrayIcon trayIcon;
+    private final SystemTray tray = SystemTray.getSystemTray();
+
+    public static Ping create() throws IOException, IconManagerException {
+        checkSystemTraySupported();
+        setLookAndFeel();
+        initIconManager();
+        return new Ping();
+    }
+
+    private static void checkSystemTraySupported() {
+        if (!SystemTray.isSupported())
+            throw new RuntimeException("SystemTray is not supported");
+    }
+
+    private Ping() throws IOException, IconManagerException {
+        trayIcon = new TrayIcon(imageOff);
+        trayIcon.setImageAutoSize(true);
+        trayIcon.setToolTip("util");
+        trayIcon.setActionCommand(CMD_MAIN);
+    }
+
+    private static void setLookAndFeel() {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 //            UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
-//            //UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
-//        } catch(UnsupportedLookAndFeelException ex) {
-//            ex.printStackTrace();
-//        } catch(IllegalAccessException ex) {
-//            ex.printStackTrace();
-//        } catch(InstantiationException ex) {
-//            ex.printStackTrace();
-//        } catch(ClassNotFoundException ex) {
-//            ex.printStackTrace();
-//        }
+//            UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
         /* Turn off metal's use of bold fonts */
         UIManager.put("swing.boldMetal", Boolean.FALSE);
-        //Schedule a job for the event-dispatching thread:
-        //adding TrayIcon.
-        SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
+    }
+
+    private static void initIconManager() throws IOException, IconManagerException {
+        IconManager iconManager = IconManager.getInstance();
+
+        try (InputStream in = Ping.class.getResourceAsStream("/img/on.ico")) {
+            iconManager.addIcon(ID_ON, ImageIO.createImageInputStream(in));
+        }
+
+        try (InputStream in = Ping.class.getResourceAsStream("/img/off.ico")) {
+            iconManager.addIcon(ID_OFF, ImageIO.createImageInputStream(in));
+        }
+    }
+
+    private void setVisible() throws IOException, IconManagerException, AWTException {
+        new TaskDaemon().start();
+        trayIcon.setPopupMenu(createPopupMenu());
+        trayIcon.addActionListener(this);
+        tray.add(trayIcon);
+    }
+
+    private PopupMenu createPopupMenu() {
+        PopupMenu popup = new PopupMenu();
+        popup.add(createMenuItem("About", CMD_MENU_ABOUT));
+        popup.addSeparator();
+        popup.add(createMenuDisplay());
+        popup.add(createMenuItem("Exit", CMD_MENU_EXIT));
+        return popup;
+    }
+
+    private Menu createMenuDisplay() {
+        Menu menu = new Menu("Display");
+        menu.add(createMenuItem("Error", CMD_MENU_ERROR));
+        menu.add(createMenuItem("Warning", CMD_MENU_WARNING));
+        menu.add(createMenuItem("Info", CMD_MENU_INFO));
+        menu.add(createMenuItem("None", CMD_MENU_NONE));
+        return menu;
+    }
+
+    private MenuItem createMenuItem(String title, String command) {
+        MenuItem menuItem = new MenuItem(title);
+        menuItem.setActionCommand(command);
+        menuItem.addActionListener(this);
+        return menuItem;
+    }
+
+    private static Image createImage(String id) throws IconManagerException, IOException {
+        IconFile iconFile = IconManager.getInstance().getIconFile(id);
+        return new ImageIcon(iconFile.getImage(iconFile.getIds().iterator().next())).getImage();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String command = e.getActionCommand();
+
+        if (CMD_MENU_ABOUT.equals(command))
+            JOptionPane.showMessageDialog(null, "This dialog box is run from the About menu item");
+        else if (CMD_MENU_ERROR.equals(command))
+            trayIcon.displayMessage("Sun TrayIcon Demo", "This is an error message", TrayIcon.MessageType.ERROR);
+        else if (CMD_MENU_WARNING.equals(command))
+            trayIcon.displayMessage("Sun TrayIcon Demo", "This is a warning message", TrayIcon.MessageType.WARNING);
+        else if (CMD_MENU_INFO.equals(command))
+            trayIcon.displayMessage("Sun TrayIcon Demo", "This is an info message", TrayIcon.MessageType.INFO);
+        else if (CMD_MENU_NONE.equals(command))
+            trayIcon.displayMessage("Sun TrayIcon Demo", "This is an ordinary message", TrayIcon.MessageType.NONE);
+        else if (CMD_MENU_EXIT.equals(command)) {
+            tray.remove(trayIcon);
+            System.exit(0);
+        } else if (CMD_MAIN.equals(command))
+            JOptionPane.showMessageDialog(null, "This dialog box is run from System Tray");
+
+    }
+
+    private final class TaskDaemon extends Thread {
+
+        private final long interval = TimeUnit.SECONDS.toMillis(5);
+        private final int connectTimeout = (int)TimeUnit.SECONDS.toMillis(5);
+        private final int readTimeout = (int)TimeUnit.SECONDS.toMillis(5);
+
+        public TaskDaemon() {
+            super("ping-task-daemon");
+            setDaemon(true);
+        }
+
+        private void checkStatus() {
+            try {
+//                    URL url = new URL("http://trialingutil.exp-design-stage.apps.usae-2.syngentaaws.org/health");
+                URL url = new URL("http://localhost:8081/health");
+                HttpURLConnection con = (HttpURLConnection)url.openConnection();
+                con.setRequestMethod("GET");
+                con.setConnectTimeout(connectTimeout);
+                con.setReadTimeout(readTimeout);
+
+                int status = con.getResponseCode();
+                trayIcon.setImage(status == SC_OK ? imageOn : imageOff);
+            } catch(Exception e) {
+                trayIcon.setImage(imageOff);
+            }
+        }
+
+        @Override
+        public void run() {
+            while (true) {
                 try {
-                    createAndShowGUI();
-                } catch(IOException e) {
-                    e.printStackTrace();
-                } catch(IconManagerException e) {
-                    e.printStackTrace();
+                    checkStatus();
+                    Thread.sleep(interval);
+                } catch(InterruptedException e) {
+                    return;
                 }
             }
-        });
-    }
-
-    private static void createAndShowGUI() throws IOException, IconManagerException {
-        //Check the SystemTray support
-        if (!SystemTray.isSupported()) {
-            System.out.println("SystemTray is not supported");
-            return;
         }
-        final PopupMenu popup = new PopupMenu();
-        final TrayIcon trayIcon = new TrayIcon(createImage("/old/Icon/0419/1.ico", "tray icon"));
-        final SystemTray tray = SystemTray.getSystemTray();
+    }
 
-        trayIcon.setImageAutoSize(true);;
-
-        // Create a popup menu components
-        MenuItem aboutItem = new MenuItem("About");
-        CheckboxMenuItem cb1 = new CheckboxMenuItem("Set auto size");
-        CheckboxMenuItem cb2 = new CheckboxMenuItem("Set tooltip");
-        Menu displayMenu = new Menu("Display");
-        MenuItem errorItem = new MenuItem("Error");
-        MenuItem warningItem = new MenuItem("Warning");
-        MenuItem infoItem = new MenuItem("Info");
-        MenuItem noneItem = new MenuItem("None");
-        MenuItem exitItem = new MenuItem("Exit");
-
-        //Add components to popup menu
-        popup.add(aboutItem);
-        popup.addSeparator();
-        popup.add(cb1);
-        popup.add(cb2);
-        popup.addSeparator();
-        popup.add(displayMenu);
-        displayMenu.add(errorItem);
-        displayMenu.add(warningItem);
-        displayMenu.add(infoItem);
-        displayMenu.add(noneItem);
-        popup.add(exitItem);
-
-        trayIcon.setPopupMenu(popup);
-
-        try {
-            tray.add(trayIcon);
-        } catch(AWTException e) {
-            System.out.println("TrayIcon could not be added.");
-            return;
-        }
-
-        trayIcon.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(null,
-                        "This dialog box is run from System Tray");
-            }
-        });
-
-        aboutItem.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(null,
-                        "This dialog box is run from the About menu item");
-            }
-        });
-
-        cb1.addItemListener(new ItemListener() {
-            public void itemStateChanged(ItemEvent e) {
-                int cb1Id = e.getStateChange();
-                if (cb1Id == ItemEvent.SELECTED) {
-                    trayIcon.setImageAutoSize(true);
-                } else {
-                    trayIcon.setImageAutoSize(false);
-                }
-            }
-        });
-
-        cb2.addItemListener(new ItemListener() {
-            public void itemStateChanged(ItemEvent e) {
-                int cb2Id = e.getStateChange();
-                if (cb2Id == ItemEvent.SELECTED) {
-                    trayIcon.setToolTip("Sun TrayIcon");
-                } else {
-                    trayIcon.setToolTip(null);
-                }
-            }
-        });
-
-        ActionListener listener = new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                MenuItem item = (MenuItem)e.getSource();
-                //TrayIcon.MessageType type = null;
-                System.out.println(item.getLabel());
-                if ("Error".equals(item.getLabel())) {
-                    //type = TrayIcon.MessageType.ERROR;
-                    trayIcon.displayMessage("Sun TrayIcon Demo",
-                            "This is an error message", TrayIcon.MessageType.ERROR);
-
-                } else if ("Warning".equals(item.getLabel())) {
-                    //type = TrayIcon.MessageType.WARNING;
-                    trayIcon.displayMessage("Sun TrayIcon Demo",
-                            "This is a warning message", TrayIcon.MessageType.WARNING);
-
-                } else if ("Info".equals(item.getLabel())) {
-                    //type = TrayIcon.MessageType.INFO;
-                    trayIcon.displayMessage("Sun TrayIcon Demo",
-                            "This is an info message", TrayIcon.MessageType.INFO);
-
-                } else if ("None".equals(item.getLabel())) {
-                    //type = TrayIcon.MessageType.NONE;
-                    trayIcon.displayMessage("Sun TrayIcon Demo",
-                            "This is an ordinary message", TrayIcon.MessageType.NONE);
-                }
-            }
-        };
-
-        errorItem.addActionListener(listener);
-        warningItem.addActionListener(listener);
-        infoItem.addActionListener(listener);
-        noneItem.addActionListener(listener);
-
-        exitItem.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                tray.remove(trayIcon);
-                System.exit(0);
+    public static void main(String... args) {
+        SwingUtilities.invokeLater(() -> {
+            try {
+                create().setVisible();
+            } catch(Exception e) {
+                e.printStackTrace();
             }
         });
     }
-
-    //Obtain the image URL
-    protected static Image createImage(String path, String description) throws IconManagerException, IOException {
-        String id = "on";
-        iconManager.addIcon(id, ImageIO.createImageInputStream(Ping.class.getResourceAsStream(path)));
-
-        IconFile iconFile = iconManager.getIconFile(id);
-
-        return new ImageIcon(iconFile.getImage("32x32_5")).getImage();
-    }
-
 }
